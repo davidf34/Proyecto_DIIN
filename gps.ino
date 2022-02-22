2728c9819eb8996960751b933121f123bed24059
David Sandoval <david.sandoval-g@mail.escuelaing.edu.co>
2022-02-21 19:29:21 -0500

    puse hola xd

--- gps.ino
+++ gps.ino
@@ -1,7 +1,7 @@
 #include <Wire.h>
 #include <Adafruit_SSD1306.h>
 #include <TinyGPS++.h>
-
+//Hola cambios 1
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
