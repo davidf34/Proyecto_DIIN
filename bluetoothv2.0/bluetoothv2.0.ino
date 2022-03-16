4e8c65b1551491ad740abbfa56e2b567b883e075
cris318 <cristian.rincon-m@mail.escuelaing.edu.co>
2022-03-15 23:19:18 -0500

    Creacion proyecto en Altium y Pruebas de Bluethooh Con el Ing.David Sandoval XD

---
+++ bluetoothv2.0/bluetoothv2.0.ino
@@ -0,0 +1,127 @@
+/*
+  Program to control LED (ON/OFF) from ESP32 using Serial Bluetooth
+  by Daniel Carrasco -> https://www.electrosoftcloud.com/
+*/
+#include "BluetoothSerial.h"
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+#define LED 23
+#define LED_OFF 21
+int band=20;
+BluetoothSerial BT; // Objeto Bluetooth
+void callback_function(esp_spp_cb_event_t event, esp_spp_cb_param_t *param) {
+ /* if (event == ESP_SPP_START_EVT) {
+    Serial.println("Inicializado SPP");
+  }*/
+ if (event == ESP_SPP_SRV_OPEN_EVT ) {
+    Serial.println("Cliente conectado");
+    BT.println("1");
+    digitalWrite(LED,HIGH);
+    delay(1000);
+    digitalWrite(LED,LOW);
+    delay(500);
+    digitalWrite(LED,HIGH);
+    delay(1000);
+    digitalWrite(LED,LOW);
+    band=0;
+    
+    while(band==0){
+      digitalWrite(LED,HIGH);
+      delay(1000);
+      digitalWrite(LED,LOW);
+      delay(1000);
+      esp_spp_cb_event_t event;
+      band2=1;
+      if(event == ESP_SPP_CLOSE_EVT){
+        band=1;
+        Serial.println("ya no Valimos VERGA");
+        delay(1000);
+        }  
+        Serial.println("Valimos BERGA");
+    }
+    
+  }
+  else if (event == ESP_SPP_CLOSE_EVT  ) {
+    Serial.println("Cliente desconectado");
+    digitalWrite(LED_OFF,HIGH);
+    delay(3000);
+    digitalWrite(LED_OFF,LOW);
+    delay(500);
+    digitalWrite(LED_OFF,HIGH);
+    delay(3000);
+    digitalWrite(LED_OFF,LOW);
+    band=1; 
+  }
+//    while(band==1){
+//    digitalWrite(LED_OFF,HIGH);
+//    delay(1000);                      //Estoy desconectado
+//    digitalWrite(LED_OFF,LOW);
+//    if (event == ESP_SPP_SRV_OPEN_EVT  ) {
+//      band=0;
+//      }
+//    }
+//    while(band==0){
+//      digitalWrite(LED,HIGH);
+//      delay(500);
+//      digitalWrite(LED,LOW);
+//      if (event == ESP_SPP_CLOSE_EVT  ) {
+//        band=1;
+//        }
+//    }
+}
+  /*else if (event == ESP_SPP_DATA_IND_EVT ) {
+    Serial.println("Datos recibidos");
+    /*while (BT.available()) { // Mientras haya datos por recibir
+      int incoming = BT.read(); // Lee un byte de los datos recibidos
+      Serial.print("Recibido: ");
+      Serial.println(incoming);
+      if (incoming == 49) { // 1 en ASCII
+        digitalWrite(LED, HIGH); // Encender el LED
+        BT.println("LED encendido"); // Envía el texto a través del puerto Serial del BT
+      }
+      else if (incoming == 48) { // 0 en ASCII
+        digitalWrite(LED, LOW); // Apagar el LED
+        BT.println("LED apagado"); // Envía el texto a través del puerto Serial del BT
+      }
+    }
+  }*/
+
+void setup() {
+  Serial.begin(115200); // Inicializando la conexión serial para debug
+  BT.begin("ESP32_LED_Control"); // Nombre de tu Dispositivo Bluetooth y en modo esclavo
+  Serial.println("El dispositivo Bluetooth está listo para emparejar");
+  BT.register_callback(callback_function); // Registramos la función "callback_function" como función callback.
+  pinMode (LED, OUTPUT); // Cambia el PIN del led a OUTPUT
+  pinMode (LED_OFF, OUTPUT);
+  
+}
+void loop() {
+  esp_spp_cb_event_t event;
+//  while(event == ESP_SPP_SRV_OPEN_EVT ){
+//    Serial.println("Cliente conectado");
+//    BT.println("1");
+//    digitalWrite(LED,HIGH);//1
+//    delay(1000);
+//    digitalWrite(LED,LOW);
+//    delay(500);
+//    digitalWrite(LED,HIGH);//2
+//    delay(1000);
+//    digitalWrite(LED,LOW);
+//    delay(500);
+//    digitalWrite(LED,HIGH);//3
+//    delay(1000);
+//    digitalWrite(LED,LOW);
+//    }
+//  while(event == ESP_SPP_CLOSE_EVT ){
+//    Serial.println("Cliente desconectado");
+//    digitalWrite(LED_OFF,HIGH);//1
+//    delay(1000);
+//    digitalWrite(LED_OFF,LOW);
+//    delay(500);
+//    digitalWrite(LED_OFF,HIGH);//2
+//    delay(1000);
+//    digitalWrite(LED_OFF,LOW);
+//    delay(500);
+//    }
+}
