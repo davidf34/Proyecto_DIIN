b573869c3a28e53bfacd4dc5eb365a08c7e0aff0
David Felipe Sandoval Granados <69484071+davidf34@users.noreply.github.com>
2022-04-22 23:47:13 -0500

    Update bluetoothv2.0.ino

--- bluetoothv2.0.ino
+++ bluetoothv2.0.ino
@@ -8,25 +8,49 @@
 #endif
 #define LED_CONECTADO 23
 #define LED_DESCONECTADO 19
+#define MOTOR_ON  10
+#define MOTOR_OFF 11
+#define Alarma 2
+
+
 int band_on;
 int band_off;
+int band_rta;
 BluetoothSerial BT; // Objeto Bluetooth
+
+
 void callback_function(esp_spp_cb_event_t event, esp_spp_cb_param_t *param) {
 if (event == ESP_SPP_START_EVT) {
     Serial.println("Inicializado ESP");
   }
 if (event == ESP_SPP_SRV_OPEN_EVT ) {
+        int rta;
         band_on = 1;
         band_off=0;
         Serial.println("Cliente conectado");
         BT.println("1");
         digitalWrite(LED_DESCONECTADO,LOW);
         digitalWrite(LED_CONECTADO,HIGH);
+        
+        BT.println("¡Bienvenido al sistema de seguridad para bicicletas!");
+        BT.println("¿Desea asegurar su bicicleta? (Si/No)");
+        rta = BT.read(); 
+        if(rta == "Si"){
+          band_on=1;
+        }
+        else if(rta == "No"){
+         // band_off=1;
+          band_rta =1;
+        }
+        else{
+           BT.println("Por favor ingrese una respuesta válida.");
+        }
+        
         } 
 
-else if (event == ESP_SPP_CLOSE_EVT){
-        band_on = 0;
-        band_off=1;
+else if (event == ESP_SPP_CLOSE_EVT && band_on==1){
+        band_alarma = 1;
+        
         Serial.println("Cliente desconectado");
         BT.println("0");
         digitalWrite(LED_CONECTADO,LOW);
@@ -58,8 +82,32 @@ void setup() {
   BT.register_callback(callback_function); // Registramos la función "callback_function" como función callback.
   pinMode (LED_CONECTADO, OUTPUT); // Cambia el PIN del led a OUTPUT
   pinMode (LED_DESCONECTADO, OUTPUT);
+  pinMode (MOTOR_ON, OUTPUT);
+  pinMode (MOTOR_OFF, OUTPUT);
+  pinMode (Alarma, OUTPUT);
   
 }
 void loop() {
-
+  //PARA ENROLLAR EL MOTOR
+  if (band_off == 1){
+    
+    digitalWrite(MOTOR_ON,HIGH);
+    delay(2000);//Tiempo en el que se va  a enrrollar el motor, hay que hacer pruebas para saber cuanto tiempo se puede estimar
+    digitalWrite(MOTOR_ON,LOW);
+  }
+  //PARA DESENRROLLAR EL MOTOR
+  else if(band_on == 1){
+    digitalWrite(MOTOR_OFF, HIGH);
+    delay(2000);
+    digitalWrite(MOTOR_OFF,LOW);
+    
+  }
+  else if(band_rta == 1){
+    BT.println("¡Peligro!, seguridad desactivada.");
+  }
+  else if(band_alarma == 1){
+     digitalWrite(Alarma,HIGH);
+  }
+  
+  
 }
